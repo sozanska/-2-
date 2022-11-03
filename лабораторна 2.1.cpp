6a3981fee38a00723b627d300fb29658b3253921
sozanska <116510991+sozanska@users.noreply.github.com>
2022-11-03 14:53:34 +0200

    Add files via upload

---
+++ лабораторна 2.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+	z1 = (sin(a) + sin(5 * a) - sin(3 * a)) /
+		(cos(a) - cos(3 * a) + cos(5 * a));
+	z2 = tan(3 * a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
